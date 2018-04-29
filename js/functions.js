4f5109fbf56893ea916fab33fa4c9978a7a24f39
fakham <mohammed.fakham@gmail.com>
2018-04-29 23:19:48 +0100

    adding JS

---
+++ js/functions.js
@@ -0,0 +1,83 @@
+function ajouterUtilisateur() {
+
+}
+
+function modifierUtilisateur() {
+
+}
+
+function supprimerUtilisateur() {
+
+}
+
+function ajouterAdmin() {
+
+}
+
+function modifierAdmin() {
+
+}
+
+function supprimerAdmin() {
+
+}
+
+function ajouterProduit() {
+
+}
+
+function modifierProduit() {
+
+}
+
+function supprimerProduit() {
+
+}
+
+function ajouterCharge() {
+
+}
+
+function modifierCharge() {
+
+}
+
+function supprimerCharge() {
+
+}
+
+function ajouterRecette() {
+
+}
+
+function modifierRecette() {
+
+}
+
+function supprimerRecette() {
+
+}
+
+function ajouterClient() {
+
+}
+
+function modifierClient() {
+
+}
+
+function supprimerClient() {
+
+}
+
+function ajouterFournisseur() {
+
+}
+
+function modifierFournisseur() {
+
+}
+
+function supprimerFournisseur() {
+    
+}
